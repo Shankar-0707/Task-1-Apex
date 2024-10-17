db46636f52197c9bd0795439076bb6e45d385636
Shankar <shankarjangid550@gmail.com>
2024-10-17 13:03:42 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// We are going to make two basic functions start() and contact() and necessary information are passed into them and then we call these functions on our button by activating onclick property and then call these function in that property.
+
+
+function start() {
+ alert("You are going to start your journey. Best of luck !!")
+}
+
+function contact() {
+    prompt("Enter your number by which you are going to Contact us :")
+}
+
+
+
